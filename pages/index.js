1ec0c49c4d788bd8e96d13c1dc206a8c32646a22
visal.naqvi <visal.naqvi@electrifai.com>
2025-06-29 17:53:10 +0530

    up

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,8 @@ export default function Home() {
 
       {/* <h2 className="subHeading center">Visit Us</h2>
       <p className="content center"><strong style={{ color: "#000" }}>Laxmi Nagar Branch:</strong> 1/53, Ist Floor, Lalita Park, Laxmi Nagar, Delhi-92</p>
-      <p className="content center"><strong style={{ color: "#000" }}>Okhla Branch:</strong> 334E, 3rd Floor, Batla House Chowk, Jamia Nagar, Okhla, Upper Rajdhani Medical Store, New Delhi, Delhi-110025</p> */}
+      <p className="content center"><strong style={{ color: "#000" }}>Okhla Branch:</strong> 334E, 3rd Floor, Batla House Chowk, Jamia Nagar, Okhla, Upper Rajdhani Medical Store, New Delhi, Delhi-110025</p>
+       */}
 
       <br></br>
     </div>
